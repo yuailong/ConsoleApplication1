3280fc89bf29a42d3d1a31e47e340dc0ecdf7177
picv888 <274009365@qq.com>
2021-04-22 16:37:13 +0700

    Update 鼠标.cpp

--- 鼠标/鼠标.cpp
+++ 鼠标/鼠标.cpp
@@ -18,24 +18,28 @@
 #define 三队_F10_虚拟码 VK_F11
 #define 四队_F10_虚拟码 VK_F12
 
+#define 切换硬直时间 250
+
 
 //当前角色
 #define SelectedCharacter_Diluke 101		//迪卢克
 #define SelectedCharacter_Keqing 102		//刻晴
 #define SelectedCharacter_Qin 103			//琴
-#define SelectedCharacter1_E_A 1001			//在1号位的，有E用E的，没E普攻的角色
-#define SelectedCharacter2_E_A 1002			//在2号位的，有E用E的，没E普攻的角色
-#define SelectedCharacter3_E_A 1003			//在3号位的，有E用E的，没E普攻的角色
-#define SelectedCharacter4_E_A 1004			//在4号位的，有E用E的，没E普攻的角色
-#define SelectedCharacter_LongE 1005		//长按E的角色
-
+#define SelectedCharacter1_E_A 1001			//在1号位的，短按E的角色
+#define SelectedCharacter2_E_A 1002			//在2号位的，短按E的角色
+#define SelectedCharacter3_E_A 1003			//在3号位的，短按E的角色
+#define SelectedCharacter4_E_A 1004			//在4号位的，短按E的角色
+#define SelectedCharacter1_LongE 1101		///在1号位的，长按E的角色
+#define SelectedCharacter2_LongE 1102		///在2号位的，长按E的角色
+#define SelectedCharacter3_LongE 1103		///在3号位的，长按E的角色
+#define SelectedCharacter4_LongE 1104		///在4号位的，长按E的角色
 
 
 bool kaiguan = 1;
 int selectedCharacterBefore = SelectedCharacter1_E_A;
 int selectedCharacterAfter = SelectedCharacter1_E_A;
 int team1[4] = { SelectedCharacter1_E_A, SelectedCharacter2_E_A, SelectedCharacter3_E_A, SelectedCharacter_Diluke };
-int team2[4] = { SelectedCharacter1_E_A, SelectedCharacter_Qin, SelectedCharacter_Keqing, SelectedCharacter_LongE };
+int team2[4] = { SelectedCharacter1_E_A, SelectedCharacter_Qin, SelectedCharacter_Keqing, SelectedCharacter4_LongE };
 int team3[4] = { SelectedCharacter1_E_A, SelectedCharacter2_E_A, SelectedCharacter3_E_A, SelectedCharacter4_E_A };
 int team4[4] = { SelectedCharacter1_E_A, SelectedCharacter2_E_A, SelectedCharacter3_E_A, SelectedCharacter4_E_A };
 int(*pSelectedTeam)[4] = &team1;
@@ -67,25 +71,34 @@ SHORT team3KeyStateAfter = 0;
 SHORT team4KeyStateAfter = 0;
 SHORT mouseSideKey2StateAfter = 0;
 
-void keyDown(int willSelecteCharacter);
-void keyHold(int willSelecteCharacter);
-void keyUp(int willSelecteCharacter);
+void keyDown(int selectedCharacterAfter);
+void keyHold(int selectedCharacterAfter);
+void keyUp(int selectedCharacterAfter);
 
-void keqingDown(int willSelecteCharacter);//按下
-void keqingHold(int willSelecteCharacter);//按住
-void keqingUp(int willSelecteCharacter);//弹起
+//刻晴
+void keqingDown(int selectedCharacterAfter);//按下
+void keqingHold(int selectedCharacterAfter);//按住
+void keqingUp(int selectedCharacterAfter);//弹起
 
-void longE_Down(int willSelecteCharacter);//按下
-void longE_Hold(int willSelecteCharacter);//按住
-void longE_Up(int willSelecteCharacter);//弹起
+//琴
+void qinDown(int selectedCharacterAfter);
+void qinHold(int selectedCharacterAfter);
+void qinUp(int selectedCharacterAfter);
 
-void dilukeDown(int willSelecteCharacter);//按下
-void dilukeHold(int willSelecteCharacter);//按住
-void dilukeUp(int willSelecteCharacter);//弹起
+//长按E的角色
+void longE_Down(int selectedCharacterAfter);//按下
+void longE_Hold(int selectedCharacterAfter);//按住
+void longE_Up(int selectedCharacterAfter);//弹起
 
-void E_A_Down(int willSelecteCharacter);//按下
-void E_A_Hold(int willSelecteCharacter);//按住
-void E_A_Up(int willSelecteCharacter);//弹起
+//迪卢克
+void dilukeDown(int selectedCharacterAfter);//按下
+void dilukeHold(int selectedCharacterAfter);//按住
+void dilukeUp(int selectedCharacterAfter);//弹起
+
+//短按E的角色
+void E_A_Down(int selectedCharacterAfter);//按下
+void E_A_Hold(int selectedCharacterAfter);//按住
+void E_A_Up(int selectedCharacterAfter);//弹起
 
 int main(){
 	/*
@@ -146,8 +159,7 @@ int main(){
 		else if(key1StateBefore && !key1StateAfter){
 			keyUp(selectedCharacterAfter);
 		}
-
-		if(!key2StateBefore && key2StateAfter){
+		else if(!key2StateBefore && key2StateAfter){
 			selectedCharacterAfter = (*pSelectedTeam)[1];
 			keyDown(selectedCharacterAfter);
 		}
@@ -157,8 +169,7 @@ int main(){
 		else if(key2StateBefore && !key2StateAfter){
 			keyUp(selectedCharacterAfter);
 		}
-
-		if(!key3StateBefore && key3StateAfter){
+		else if(!key3StateBefore && key3StateAfter){
 			selectedCharacterAfter = (*pSelectedTeam)[2];
 			keyDown(selectedCharacterAfter);
 		}
@@ -168,8 +179,7 @@ int main(){
 		else if(key3StateBefore && !key3StateAfter){
 			keyUp(selectedCharacterAfter);
 		}
-
-		if(!key4StateBefore && key4StateAfter){
+		else if(!key4StateBefore && key4StateAfter){
 			selectedCharacterAfter = (*pSelectedTeam)[3];
 			keyDown(selectedCharacterAfter);
 		}
@@ -179,8 +189,7 @@ int main(){
 		else if(key4StateBefore && !key4StateAfter){
 			keyUp(selectedCharacterAfter);
 		}
-
-		if(!mouseSideKey2StateBefore && mouseSideKey2StateAfter){
+		else if(!mouseSideKey2StateBefore && mouseSideKey2StateAfter){
 			keyDown(selectedCharacterAfter);
 		}
 		else if(mouseSideKey2StateBefore && mouseSideKey2StateAfter){
@@ -203,82 +212,92 @@ int main(){
 	}
 }
 
-void keyDown(int willSelecteCharacter){
-	switch(willSelecteCharacter){
+void keyDown(int selectedCharacterAfter){
+	switch(selectedCharacterAfter){
 	case SelectedCharacter_Keqing:
-		keqingDown(willSelecteCharacter);
+		keqingDown(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter_Diluke:
-		dilukeDown(willSelecteCharacter);
+		dilukeDown(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter_Qin:
-		keqingDown(willSelecteCharacter);
+		qinDown(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter1_E_A:
 	case SelectedCharacter2_E_A:
 	case SelectedCharacter3_E_A:
 	case SelectedCharacter4_E_A:
-		E_A_Down(willSelecteCharacter);
+		E_A_Down(selectedCharacterAfter);
 		break;
 
-	case SelectedCharacter_LongE:
-		longE_Down(willSelecteCharacter);
+	case SelectedCharacter1_LongE:
+	case SelectedCharacter2_LongE:
+	case SelectedCharacter3_LongE:
+	case SelectedCharacter4_LongE:
+		longE_Down(selectedCharacterAfter);
 		break;
 	}
 }
 
-void keyHold(int willSelecteCharacter){
-	switch(willSelecteCharacter){
+void keyHold(int selectedCharacterAfter){
+	switch(selectedCharacterAfter){
 	case SelectedCharacter_Keqing:
-		keqingHold(willSelecteCharacter);
+		keqingHold(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter_Diluke:
-		dilukeHold(willSelecteCharacter);
+		dilukeHold(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter_Qin:
-		keqingHold(willSelecteCharacter);
+		qinHold(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter1_E_A:
 	case SelectedCharacter2_E_A:
 	case SelectedCharacter3_E_A:
 	case SelectedCharacter4_E_A:
-		E_A_Hold(willSelecteCharacter);
+		E_A_Hold(selectedCharacterAfter);
 		break;
 
-	case SelectedCharacter_LongE:
-		longE_Hold(willSelecteCharacter);
+	case SelectedCharacter1_LongE:
+	case SelectedCharacter2_LongE:
+	case SelectedCharacter3_LongE:
+	case SelectedCharacter4_LongE:
+		longE_Hold(selectedCharacterAfter);
 		break;
 	}
 }
 
-void keyUp(int willSelecteCharacter){
-	switch(willSelecteCharacter){
+void keyUp(int selectedCharacterAfter){
+	switch(selectedCharacterAfter){
 	case SelectedCharacter_Keqing:
-		keqingUp(willSelecteCharacter);
+		keqingUp(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter_Diluke:
-		dilukeUp(willSelecteCharacter);
+		dilukeUp(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter_Qin:
-		keqingUp(willSelecteCharacter);
+		qinUp(selectedCharacterAfter);
 		break;
 
 	case SelectedCharacter1_E_A:
 	case SelectedCharacter2_E_A:
 	case SelectedCharacter3_E_A:
 	case SelectedCharacter4_E_A:
-		E_A_Up(willSelecteCharacter);
+		E_A_Up(selectedCharacterAfter);
 		break;
-	case SelectedCharacter_LongE:
-		longE_Up(willSelecteCharacter);
+
+	case SelectedCharacter1_LongE:
+	case SelectedCharacter2_LongE:
+	case SelectedCharacter3_LongE:
+	case SelectedCharacter4_LongE:
+		longE_Up(selectedCharacterAfter);
 		break;
 	}
 }
@@ -287,96 +306,165 @@ void keyUp(int willSelecteCharacter){
 
 
 
-#define 步骤_刻晴一段E 1
-#define 步骤_刻晴一段E等待后摇 2
-#define 步骤_刻晴二段E 3
-#define 步骤_刻晴二段E等待后摇 4
-#define 步骤_刻晴重击按下鼠标 5
-#define 步骤_刻晴重击等待或者E 6
-#define 步骤_刻晴重击等待后摇 7
+#define 刻晴步骤1_E 1
+#define 刻晴步骤2_等待50毫秒 2
+#define 刻晴步骤3_按下左键 3
+#define 刻晴步骤4_如果重击完毕则弹起鼠标跳到第7步_否则等待50毫秒 4
+#define 刻晴步骤5_如果重击完毕则弹起鼠标跳到第7步_否则E 5
+#define 刻晴步骤6_如果重击完毕则弹起鼠标跳到第7步_否则等待50毫秒回到第5步 6
+#define 刻晴步骤7_等待50毫秒回到第3步 7
 
 
 #define 刻晴重击按住时间 400
 #define 刻晴重击松开鼠标后等待时间 100
 
-int keqingBuzhou = 步骤_刻晴一段E;
-DWORD changeKeqingTime = 0;
-DWORD keqingFirst_E_Time = 0;
-DWORD keqingSecond_E_Time = 0;
-DWORD keqing_Down_Time = 0;
-DWORD keqing_Up_Time = 0;
+int keqingBuzhou = 刻晴步骤1_E;
+DWORD changeKeqingTime = 0;//切换到刻晴的时间
+DWORD keqing_E_Time = 0;
+DWORD keqing_MouseDown_Time = 0;
+DWORD keqing_MouseUp_Time = 0;
 
-void keqingDown(int willSelecteCharacter){
-	if(selectedCharacterBefore != willSelecteCharacter){
-		keqingBuzhou = 步骤_刻晴一段E;
+void keqingDown(int selectedCharacterAfter){
+	if(selectedCharacterBefore != selectedCharacterAfter){
 		changeKeqingTime = timeGetTime();
 	}
+	keqingBuzhou = 刻晴步骤1_E;
 }
 
-void keqingHold(int willSelecteCharacter){
+void keqingHold(int selectedCharacterAfter){
 	DWORD nowTime = timeGetTime();
 
 	//等待切换硬直时间
-	if(nowTime - changeKeqingTime < 250){
+	if(nowTime - changeKeqingTime < 切换硬直时间){
 		return;
 	}
 
 	switch(keqingBuzhou){
-	case 步骤_刻晴一段E:
+	case 刻晴步骤1_E:
 		keybd_event(E键虚拟码, E键扫描码, 0, 0);//按下E键
 		keybd_event(E键虚拟码, E键扫描码, KEYEVENTF_KEYUP, 0);//弹起E键
-		keqingBuzhou = 步骤_刻晴一段E等待后摇;
-		keqingFirst_E_Time = nowTime;
+		keqing_E_Time = nowTime;
+		keqingBuzhou = 刻晴步骤2_等待50毫秒;
 		break;
 
-	case 步骤_刻晴一段E等待后摇:
-		if(nowTime - keqingFirst_E_Time > 50){
-			keqingBuzhou = 步骤_刻晴重击按下鼠标;
+	case 刻晴步骤2_等待50毫秒:
+		if(nowTime - keqing_E_Time >= 50){
+			keqingBuzhou = 刻晴步骤3_按下左键;
 		}
 		break;
 
-	case 步骤_刻晴重击按下鼠标:
+	case 刻晴步骤3_按下左键:
 		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);//按下鼠标左键
-		keqingBuzhou = 步骤_刻晴重击等待或者E;
-		keqing_Down_Time = nowTime;
+		keqing_MouseDown_Time = nowTime;
+		keqingBuzhou = 刻晴步骤4_如果重击完毕则弹起鼠标跳到第7步_否则等待50毫秒;
 		break;
 
-	case 步骤_刻晴重击等待或者E:
-		if(nowTime - keqing_Down_Time >= 刻晴重击按住时间){
-			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//松开鼠标左键
-			keqingBuzhou = 步骤_刻晴重击等待后摇;
-			keqing_Up_Time = nowTime;
+	case 刻晴步骤4_如果重击完毕则弹起鼠标跳到第7步_否则等待50毫秒:
+		if(nowTime - keqing_MouseDown_Time >= 刻晴重击按住时间){
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//弹起鼠标左键
+			keqing_MouseUp_Time = nowTime;
+			keqingBuzhou = 刻晴步骤7_等待50毫秒回到第3步;
 		}
-		else if(nowTime - keqingFirst_E_Time >= 50){
-			keqingBuzhou = 步骤_刻晴二段E;
-		}
-		break;
-
-	case 步骤_刻晴重击等待后摇:
-		if(nowTime - keqing_Up_Time >= 刻晴重击松开鼠标后等待时间){
-			keqingBuzhou = 步骤_刻晴重击按下鼠标;
+		else if(nowTime - keqing_MouseDown_Time > 50){
+			keqingBuzhou = 刻晴步骤5_如果重击完毕则弹起鼠标跳到第7步_否则E;
 		}
 		break;
 
-	case 步骤_刻晴二段E:
-		keybd_event(E键虚拟码, E键扫描码, 0, 0);//按下E键
-		keybd_event(E键虚拟码, E键扫描码, KEYEVENTF_KEYUP, 0);//弹起E键
-		keqingBuzhou = 步骤_刻晴二段E等待后摇;
-		keqingFirst_E_Time = nowTime;
+	case 刻晴步骤5_如果重击完毕则弹起鼠标跳到第7步_否则E:
+		if(nowTime - keqing_MouseDown_Time >= 刻晴重击按住时间){
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//弹起鼠标左键
+			keqing_MouseUp_Time = nowTime;
+			keqingBuzhou = 刻晴步骤7_等待50毫秒回到第3步;
+		}
+		else{
+			keybd_event(E键虚拟码, E键扫描码, 0, 0);//按下E键
+			keybd_event(E键虚拟码, E键扫描码, KEYEVENTF_KEYUP, 0);//弹起E键
+			keqing_E_Time = nowTime;
+			keqingBuzhou = 刻晴步骤6_如果重击完毕则弹起鼠标跳到第7步_否则等待50毫秒回到第5步;
+		}
 		break;
 
-	case 步骤_刻晴二段E等待后摇:
-		if(nowTime - keqingFirst_E_Time > 50){
-			keqingBuzhou = 步骤_刻晴重击等待或者E;
+	case 刻晴步骤6_如果重击完毕则弹起鼠标跳到第7步_否则等待50毫秒回到第5步:
+		if(nowTime - keqing_MouseDown_Time >= 刻晴重击按住时间){
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//弹起鼠标左键
+			keqing_MouseUp_Time = nowTime;
+			keqingBuzhou = 刻晴步骤7_等待50毫秒回到第3步;
+		}
+		else if(nowTime - keqing_E_Time >= 50){
+			keqingBuzhou = 刻晴步骤5_如果重击完毕则弹起鼠标跳到第7步_否则E;
+		}
+		break;
+
+	case 刻晴步骤7_等待50毫秒回到第3步:
+		if(nowTime - keqing_MouseUp_Time >= 刻晴重击松开鼠标后等待时间){
+			keqingBuzhou = 刻晴步骤3_按下左键;
 		}
 		break;
 	}
 }
 
-void keqingUp(int willSelecteCharacter){
-	if(keqingBuzhou == 步骤_刻晴重击等待或者E){
-		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//弹起鼠标左键
+void keqingUp(int selectedCharacterAfter){
+	mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//弹起鼠标左键
+}
+
+
+
+
+#define 琴步骤1_按下鼠标 1
+#define 琴步骤2_等待重击按住鼠标 2
+#define 琴步骤3_弹起鼠标 3
+#define 琴步骤4_等待100毫秒 4
+
+DWORD changeQinTime = 0;
+DWORD qinBuzhou = 0;
+DWORD qin_Buzhou1_Time = 0;
+DWORD qin_Buzhou3_Time = 0;
+
+void qinDown(int selectedCharacterAfter){
+	if(selectedCharacterBefore != selectedCharacterAfter){
+		changeQinTime = timeGetTime();
 	}
+	qinBuzhou = 琴步骤1_按下鼠标;
+}
+
+void qinHold(int selectedCharacterAfter){
+	DWORD nowTime = timeGetTime();
+
+	//等待切换硬直时间
+	if(nowTime - changeQinTime < 切换硬直时间){
+		return;
+	}
+
+	switch(qinBuzhou){
+	case 琴步骤1_按下鼠标:
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);//按下鼠标左键
+		qin_Buzhou1_Time = nowTime;
+		qinBuzhou = 琴步骤2_等待重击按住鼠标;
+		break;
+
+	case  琴步骤2_等待重击按住鼠标:
+		if(nowTime - qin_Buzhou1_Time >= 400){
+			qinBuzhou = 琴步骤3_弹起鼠标;
+		}
+		break;
+
+	case 琴步骤3_弹起鼠标:
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//弹起鼠标左键
+		qin_Buzhou3_Time = nowTime;
+		qinBuzhou = 琴步骤4_等待100毫秒;
+		break;
+
+	case 琴步骤4_等待100毫秒:
+		if(nowTime - qin_Buzhou3_Time >= 100){
+			qinBuzhou = 琴步骤1_按下鼠标;
+		}
+		break;
+	}
+}
+
+int up2time = 0;
+void qinUp(int selectedCharacterAfter){
+	mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//弹起鼠标左键
 }
 
 
@@ -384,18 +472,18 @@ void keqingUp(int willSelecteCharacter){
 DWORD longE_buzhou = 0;
 DWORD changeLong_E_Time = 0;//切换到长按E的角色的时间
 DWORD beginLong_E_Time = 0;//按下E键的时间
-void longE_Down(int willSelecteCharacter){
-	if(selectedCharacterBefore != willSelecteCharacter){
+void longE_Down(int selectedCharacterAfter){
+	if(selectedCharacterBefore != selectedCharacterAfter){
 		longE_buzhou = 0;//刚切换到这个角色时从第0步开始
 		changeLong_E_Time = timeGetTime();
 	}
 }
 
-void longE_Hold(int willSelecteCharacter){
+void longE_Hold(int selectedCharacterAfter){
 	DWORD nowTime = timeGetTime();
 
 	//等待切换硬直时间
-	if(nowTime - changeLong_E_Time < 250){
+	if(nowTime - changeLong_E_Time < 切换硬直时间){
 		return;
 	}
 
@@ -403,17 +491,20 @@ void longE_Hold(int willSelecteCharacter){
 	case 0:
 		keybd_event(E键虚拟码, E键扫描码, 0, 0);//按下E键
 		beginLong_E_Time = nowTime;
-		while(nowTime - beginLong_E_Time < 500){
+		while(nowTime - beginLong_E_Time < 400){
 			nowTime = timeGetTime();
 		}
 		keybd_event(E键虚拟码, E键扫描码, KEYEVENTF_KEYUP, 0);//弹起E键
+		while(nowTime - beginLong_E_Time < 500){
+			nowTime = timeGetTime();
+		}
 		longE_buzhou = 0;
 		break;
 	}
 }
 
-void longE_Up(int willSelecteCharacter){
-	
+void longE_Up(int selectedCharacterAfter){
+
 }
 
 
@@ -426,18 +517,18 @@ DWORD changeDilukeTime = 0;//切换到迪卢克的时间
 DWORD diluke_E_or_A_Time = 0;//迪卢克上一次E或者A的时间
 DWORD diluke_E_time = 0;//迪卢克上一次E的时间
 
-void dilukeDown(int willSelecteCharacter){
-	if(selectedCharacterBefore != willSelecteCharacter){
+void dilukeDown(int selectedCharacterAfter){
+	if(selectedCharacterBefore != selectedCharacterAfter){
 		changeDilukeTime = timeGetTime();
 		diluke_E_or_A_Time = changeDilukeTime;
 	}
 }
 
-void dilukeHold(int willSelecteCharacter){
+void dilukeHold(int selectedCharacterAfter){
 	DWORD nowTime = timeGetTime();
 
 	//等待切换硬直时间
-	if(nowTime - changeDilukeTime < 250){
+	if(nowTime - changeDilukeTime < 切换硬直时间){
 		return;
 	}
 
@@ -472,7 +563,7 @@ void dilukeHold(int willSelecteCharacter){
 	}
 }
 
-void dilukeUp(int willSelecteCharacter){
+void dilukeUp(int selectedCharacterAfter){
 
 }
 
@@ -480,18 +571,18 @@ int E_A_Buzhou = 0;
 DWORD change_E_A_Time = 0;//切换到这个角色的时间
 DWORD E_Time = 0;//E的时间
 DWORD A_Time = 0;//A的时间
-void E_A_Down(int willSelecteCharacter){
-	if(selectedCharacterBefore != willSelecteCharacter){
-		selectedCharacterBefore = willSelecteCharacter;
+void E_A_Down(int selectedCharacterAfter){
+	if(selectedCharacterBefore != selectedCharacterAfter){
+		selectedCharacterBefore = selectedCharacterAfter;
 		change_E_A_Time = timeGetTime();
 	}
 	E_A_Buzhou = 0;
 }
-void E_A_Hold(int willSelecteCharacter){
+void E_A_Hold(int selectedCharacterAfter){
 	DWORD nowTime = timeGetTime();
 
 	//等待切换硬直时间
-	if(nowTime - change_E_A_Time < 250){
+	if(nowTime - change_E_A_Time < 切换硬直时间){
 		return;
 	}
 
@@ -524,6 +615,6 @@ void E_A_Hold(int willSelecteCharacter){
 	}
 }
 
-void E_A_Up(int willSelecteCharacter){
+void E_A_Up(int selectedCharacterAfter){
 
 }
