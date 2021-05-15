ec920335f7b1726272111fa766c0969d1ba32819
picv888 <274009365@qq.com>
2021-05-15 07:59:26 +0700

    Update 鼠标.cpp

--- 鼠标/鼠标.cpp
+++ 鼠标/鼠标.cpp
@@ -33,7 +33,7 @@ DWORD changeDelay = 250; //切换硬直时间
 bool isNeedModifyTeam = 0;//是否需要修改队员
 int selectedCharacterBefore = SelectedCharacter1_E_A;
 int selectedCharacterAfter = SelectedCharacter1_E_A;
-int team1[4] = { SelectedCharacter1_Q_E, SelectedCharacter2_E_A, SelectedCharacter3_Q_E, SelectedCharacter_Diluke };
+int team1[4] = { SelectedCharacter1_Q_E, SelectedCharacter2_LongE, SelectedCharacter3_Q_E, SelectedCharacter_Diluke };
 int team2[4] = { SelectedCharacter1_LongE, SelectedCharacter2_E_A, SelectedCharacter_Keqing, SelectedCharacter4_E_A };
 int team3[4] = { SelectedCharacter1_LongE, SelectedCharacter2_E_A, SelectedCharacter3_Q_E, SelectedCharacter_Diluke };
 int team4[4] = { SelectedCharacter1_LongE, SelectedCharacter2_E_A, SelectedCharacter3_Q_E, SelectedCharacter_Keli };
@@ -593,14 +593,14 @@ void longE_Up(int selectedCharacterAfter){
 
 int dilukeBuzhou = 1;
 DWORD changeDilukeTime = 0;//切换到迪卢克的时间
-DWORD diluke_E_or_A_Time = 0;//迪卢克上一次E或者A的时间
+DWORD diluke_E_or_A_Time2 = 0;
 DWORD diluke_E_Time = 0;//迪卢克上一次E的时间
 
 void dilukeDown(int selectedCharacterAfter){
 	if(selectedCharacterBefore != selectedCharacterAfter){
-		changeDilukeTime = timeGetTime();
-		diluke_E_or_A_Time = changeDilukeTime;
-		diluke_E_Time = nowTime;
+		changeDilukeTime = nowTime;
+		diluke_E_or_A_Time2 = changeDilukeTime;
+		diluke_E_Time = nowTime + changeDelay - 400;
 	}
 
 }
@@ -613,7 +613,7 @@ void dilukeHold(int selectedCharacterAfter){
 
 	switch(dilukeBuzhou){
 	case 1:
-		if(nowTime - diluke_E_Time >= 1300){
+		if(nowTime - diluke_E_Time >= 1100){
 			keybd_event(VirtualCode_E, ScanCode_E, 0, 0);//按下E键
 			keybd_event(VirtualCode_E, ScanCode_E, KEYEVENTF_KEYUP, 0);//弹起E键
 			diluke_E_Time = nowTime;
@@ -623,11 +623,11 @@ void dilukeHold(int selectedCharacterAfter){
 			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//松开鼠标左键
 		}
 		dilukeBuzhou = 2;
-		diluke_E_or_A_Time = nowTime;
+		diluke_E_or_A_Time2 = nowTime;
 		break;
 
 	case 2:
-		if(nowTime - diluke_E_or_A_Time >= 100){
+		if(nowTime - diluke_E_or_A_Time2 >= 100){
 			dilukeBuzhou = 1;
 		}
 		break;
