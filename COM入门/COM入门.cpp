c7274ed43c4bae309087a6680e28885da625436a
picv888 <274009365@qq.com>
2021-08-17 13:30:10 +0700

    Update COM入门.cpp

--- COM入门/COM入门.cpp
+++ COM入门/COM入门.cpp
@@ -4,6 +4,9 @@
 
 int main()
 {
-    IClassFactory
+    // {10C99CC1-AED1-4817-A4CE-3E2D7204CCE8}
+    static const GUID IID_YS =
+    { 0x10c99cc1, 0xaed1, 0x4817, { 0xa4, 0xce, 0x3e, 0x2d, 0x72, 0x4, 0xcc, 0xe8 } };
+
     std::cout << "Hello World!\n";
 }
